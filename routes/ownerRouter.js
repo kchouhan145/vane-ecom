1c8311857028614884e9187a1fe5c3c3e7a8f27a
kchouhan145 <k4studies@gmail.com>
2026-03-26 16:38:06 +0530

    implement user and owner creation

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 // const app = express();
+const ownerModel = require("../models/owner-model");
+const bcrypt = require('bcrypt');
 
 router.get("/",(req,res)=>{
     res.send("Welcome to owner route");
+})
+
+router.post("/create",async (req,res)=>{
+    let {fullName,email,password,picture} = req.body;
+
+    let passwordH = await bcrypt.hash(password,10);
+    if(await ownerModel.findOne()){
+        return  res.send("There is already a owener");
+    }
+
+    await ownerModel.create({
+        fullName,
+        email,
+        password:passwordH,
+        picture
+    });
+
+    res.status(201).send("Created the owner");
 })
 
 module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const userModel = require('../models/user-model');
 
 router.get('/', (req, res) => {
 	res.send('Welcome to users route');
 });
+
+router.post("/create", async (req,res) => {
+	
+	let {fullName,email,password,contact,picture} = req.body;
+
+	let passwordH = await bcrypt.hash(password,10);
+
+	if(await userModel.findOne({email}))
+	{
+		return res.send("Email is already register with another user");
+	}
+
+	await userModel.create({
+		fullName,
+		email,
+		password:passwordH,
+		contact,
+		picture
+	})
+
+	res.status(201).send("User Created!");
+
+})
 
 module.exports = router;
